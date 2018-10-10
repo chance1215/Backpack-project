ce3754a42a49e4a99a7b00d1a49b8410bc3b2a36
Jon McCarthy <jtmcc93@gmail.com>
2018-10-10 11:37:36 -0700

    create details page

--- config/routes.js
+++ config/routes.js
@@ -1,34 +1,33 @@
 //Update the name of the controller below and rename the file.
-const packersController = require("../controllers/packersController.js")
-const tripsController = require("../controllers/tripsController.js")
-const gearController = require("../controllers/gearController.js")
-const notesController = require("../controllers/notesController.js")
-module.exports = function(app){
+const packersController = require("../controllers/packersController.js");
+const tripsController = require("../controllers/tripsController.js");
+const gearController = require("../controllers/gearController.js");
+const notesController = require("../controllers/notesController.js");
+module.exports = function(app) {
+  app.get("/", packersController.index);
+  app.get("/login", packersController.showLogin);
+  app.post("/login", packersController.login);
+  app.post("/register", packersController.register);
 
-  app.get('/', packersController.index);
-  app.get('/login', packersController.showLogin);
-  app.post('/login',packersController.login)
-  app.post('/register', packersController.register);
-app.use(authenticateUser);
+  app.use(authenticateUser);
 
-  app.get('/welcome',packersController.welcome);
-  app.get('/newTrip',tripsController.newTrip);
-  app.get('/editTrip/:id',tripsController.editTrip);
+  app.get("/welcome", packersController.welcome);
+  app.get("/newTrip", tripsController.newTrip);
+  app.get("/editTrip/:id", tripsController.editTrip);
 
-  app.get('/trip/details/:id', tripsController.details)
-  app.get('/trip/backpack/:id', tripsController.backpack)
-  app.get('/trip/campground/:id', tripsController.campground)
+  app.get("/trip/details/:id", tripsController.details);
+  app.get("/trip/backpack/:id", tripsController.backpack);
+  app.get("/trip/campground/:id", tripsController.campground);
 
+  app.post("/notes/create/:trip_id/:user_id", notesController.create);
 
-
-
-  app.get('/logout',packersController.logout);
-}
-function authenticateUser(req, res, next){
-  if(!req.session.packer_id){
+  app.get("/logout", packersController.logout);
+};
+function authenticateUser(req, res, next) {
+  if (!req.session.packer_id) {
     console.log("Sorry, you don't have access to that page. Please login.");
-    res.redirect('/login');
-  }else{
+    res.redirect("/login");
+  } else {
     next();
   }
 }
--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -1,5 +1,5 @@
 const knex = require("../db/knex.js");
-const moment = require('moment');
+const moment = require("moment");
 
 //When creating a new trip use moment to format the date.
 //Example when creating a new date...
@@ -11,25 +11,64 @@ const moment = require('moment');
 // then change the html date value to formattedDate.
 
 module.exports = {
-
-  newTrip:(req, res) => {
-    res.render('newTrip');
+  newTrip: (req, res) => {
+    res.render("newTrip");
   },
 
-  editTrip:(req, res) => {
-    res.render('editTrip');
+  editTrip: (req, res) => {
+    res.render("editTrip");
   },
 
   details: (req, res) => {
-    res.render('details');
+    console.log(req.session.packer_id);
+    knex("tripsTable")
+      .where("tripsTable.id", req.params.id)
+      .then(trip => {
+        console.log(trip);
+        knex("packer_tripTable")
+          .join("packersTable", "packer_tripTable.packer_id", "packersTable.id")
+          .where("packer_tripTable.trip_id", req.params.id)
+          .then(packers => {
+            console.log(packers);
+            knex("packer_tripTable")
+              .where("packer_tripTable.trip_id", req.params.id)
+              .andWhere("packer_tripTable.role", "admin")
+              .then(admin => {
+                console.log("ADMIN", admin);
+                knex("packersTable")
+                  .where("id", req.session.packer_id)
+                  .then(user => {
+                    console.log("USER", user);
+                    knex("notesTable")
+                      .where("notesTable.trip_id", req.params.id)
+                      .join(
+                        "packersTable",
+                        "notesTable.packer_id",
+                        "packersTable.id"
+                      )
+                      .orderBy("notesTable.created_at", "desc")
+                      .then(notes => {
+                        console.log("NOTES", notes);
+                        res.render("details", {
+                          trip: trip[0],
+                          packers,
+                          admin: admin[0],
+                          user: user[0],
+                          notes,
+                          moment
+                        });
+                      });
+                  });
+              });
+          });
+      });
   },
 
   backpack: (req, res) => {
-    res.render('backpack');
+    res.render("backpack");
   },
 
   campground: (req, res) => {
-    res.render('campground');
-  },
-
-}
+    res.render("campground");
+  }
+};
