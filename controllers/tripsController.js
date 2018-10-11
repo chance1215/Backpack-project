6594df4d2d288ae62874be008ef962bf943396bd
Jon McCarthy <jtmcc93@gmail.com>
2018-10-11 09:46:19 -0700

    fix formatting on tripsContr.

--- controllers/tripsController.js
+++ controllers/tripsController.js
@@ -16,47 +16,57 @@ module.exports = {
     res.render("newTrip");
   },
 
-  createTrip:(req, res) => {
-    knex('tripsTable').join('packers', 'packers.id', req.session.packer_id).insert({
-      tripName: req.body.tripName,
-      location: req.body.location,
-      description: req.body.description,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-    }).then((results)=>{
-    res.redirect('/details/:id');
-    })
+  createTrip: (req, res) => {
+    knex("tripsTable")
+      .join("packers", "packers.id", req.session.packer_id)
+      .insert({
+        tripName: req.body.tripName,
+        location: req.body.location,
+        description: req.body.description,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate
+      })
+      .then(results => {
+        res.redirect("/details/:id");
+      });
   },
 
-  editTrip:(req, res) => {
+  editTrip: (req, res) => {
+    knex("tripsTable")
+      .where("id", req.params.id)
+      .then(tripsResults => {
+        let startDate = moment(tripsResults[0].startDate).format("YYYY-MM-DD");
+        let endDate = moment(tripsResults[0].endDate).format("YYYY-MM-DD");
 
-    knex('tripsTable')
-    .where('id', req.params.id)
-    .then((tripsResults) => {
-      let startDate = moment(tripsResults[0].startDate).format('YYYY-MM-DD');
-      let endDate = moment(tripsResults[0].endDate).format('YYYY-MM-DD');
-
-      knex('packer_tripTable')
-      .where('trip_id', req.params.id)
-      .join('packersTable', 'packersTable.id', 'packer_tripTable.packer_id')
-      .then((packer_tripResults) => {
-        res.render('editTrip', {tripsResults, packer_tripResults, startDate, endDate});
-      })
-    })
+        knex("packer_tripTable")
+          .where("trip_id", req.params.id)
+          .join("packersTable", "packersTable.id", "packer_tripTable.packer_id")
+          .then(packer_tripResults => {
+            res.render("editTrip", {
+              tripsResults,
+              packer_tripResults,
+              startDate,
+              endDate
+            });
+          });
+      });
   },
 
   updateTrip: (req, res) => {
-    knex('tripsTable')
-    .where('id', req.params.id)
-    .update({
-      tripName: req.body.tripName,
-      startDate: req.body.startDate,
-      endDate: req.body.endDate,
-      location: req.body.location,
-      description: req.body.description
-      }).then(()=>{
-      res.redirect(`/trip/details/${req.params.id}`);
-    })
+    knex("tripsTable")
+      .where("id", req.params.id)
+      .update({
+        tripName: req.body.tripName,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate,
+        location: req.body.location,
+        description: req.body.description
+      })
+      .then(() => {
+        res.redirect(`/trip/details/${req.params.id}`);
+      });
+  },
+
   details: (req, res) => {
     console.log(req.session.packer_id);
     knex("tripsTable")
@@ -103,11 +113,14 @@ module.exports = {
   },
 
   remove: (req, res) => {
-    knex('packer_tripTable').delete()
-    .where('packer_id', req.params.packer_id).andWhere('trip_id', req.params.trip_id)
-    .then(() => {
-      res.redirect(`/editTrip/${req.params.trip_id}`);
-    })
+    knex("packer_tripTable")
+      .delete()
+      .where("packer_id", req.params.packer_id)
+      .andWhere("trip_id", req.params.trip_id)
+      .then(() => {
+        res.redirect(`/editTrip/${req.params.trip_id}`);
+      });
+  },
   sendInvite: (req, res) => {
     console.log(req.body);
 
@@ -117,8 +130,6 @@ module.exports = {
     <h5>${req.body.location}</h5>
     <h5>${req.body.date}</h5>
     <p>${req.body.description}</p>
-
-    
     `;
 
     // create reusable transporter object using the default SMTP transport
